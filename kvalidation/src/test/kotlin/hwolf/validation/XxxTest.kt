8b1216f875cc2b6c6cb984a65d10119ad710d973
hwolf <heiko>
2021-12-11 13:39:26 +0100

    Test: Forbid merging broken pull request

---
+++ kvalidation/src/test/kotlin/hwolf/validation/XxxTest.kt
@@ -0,0 +1,12 @@
+package hwolf.validation
+
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Test
+
+class XxxTest {
+
+    @Test
+    fun test() {
+        fail<Nothing>()
+    }
+}
