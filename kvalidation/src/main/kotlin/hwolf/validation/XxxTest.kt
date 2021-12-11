b0e1cece671916ac7f3d918939d3e2e837449d56
hwolf <heiko>
2021-12-11 13:57:13 +0100

    Test: Forbid merging broken pull request Code analysis should fail

--- kvalidation/src/main/kotlin/hwolf/validation/XxxTest.kt
+++ kvalidation/src/main/kotlin/hwolf/validation/XxxTest.kt
@@ -1,11 +1,9 @@
 package hwolf.validation
 
-import org.junit.jupiter.api.Test
 import java.security.SecureRandom
 
 class XxxTest {
 
-    @Test
     fun test() {
         val sr = SecureRandom("abcdefghijklmnop".toByteArray(charset("us-ascii")))
         val v = sr.nextInt()
