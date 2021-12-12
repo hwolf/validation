aac6726f9f29811048e6b84dcef3366d682857b7
hwolf <heiko>
2021-12-12 05:22:48 +0100

    Add docu

--- kvalidation/src/main/kotlin/hwolf/validation/constraints/AnyConstraints.kt
+++ kvalidation/src/main/kotlin/hwolf/validation/constraints/AnyConstraints.kt
@@ -7,31 +7,42 @@ import kotlin.reflect.KProperty1
 
 // isEqual
 
+/** A constraint that validate if the value is equal to another value. */
 data class Equal<V>(val value: V) : Constraint
+
+/** A constraint that validate if the value is equal to another property within the same object. */
 data class EqualWith(val property: String) : Constraint
 
+/** Validates if the property value is equal to another value. */
 fun <V> ConstraintBuilder<V>.isEqual(value: V) =
     validate(Equal(value)) { v -> v == value }
 
+/** Validates if the property value is equal to another property within the same object. */
 fun <T, V> ExtConstraintBuilder<T, V>.isEqual(property: KProperty1<T, V?>) =
     validate(EqualWith(property.name)) { v, bean -> property(bean)?.let { it == v } ?: true }
 
 
 // isIn
 
+/** A constraint that validate if the value is equal to one of the values. */
 data class In<V : Any>(val allowedValues: Collection<V>) : Constraint
 
+/** Validates if the property value is equal to one of the values. */
 fun <V : Any> ConstraintBuilder<V>.isIn(allowedValues: Collection<V>) =
     validate(In(allowedValues)) { it in allowedValues }
 
+/** Validates if the property value is equal to one of the values. */
 fun <V : Any> ConstraintBuilder<V>.isIn(vararg allowedValues: V) = isIn(allowedValues.toList())
 
 
 // isNotIn
 
+/** A constraint that validate if the value is not equal to one of the values. */
 data class NotIn<V : Any>(val forbiddenValues: Collection<V>) : Constraint
 
+/** Validates if the property value is not equal to one of the values. */
 fun <V : Any> ConstraintBuilder<V>.isNotIn(forbiddenValues: Collection<V>) =
     validate(NotIn(forbiddenValues)) { it !in forbiddenValues }
 
+/** Validates if the property value is not equal to one of the values. */
 fun <V : Any> ConstraintBuilder<V>.isNotIn(vararg forbiddenValues: V) = isNotIn(forbiddenValues.toList())
--- kvalidation/src/main/kotlin/hwolf/validation/constraints/CollectionConstraints.kt
+++ kvalidation/src/main/kotlin/hwolf/validation/constraints/CollectionConstraints.kt
@@ -1,4 +1,5 @@
 @file:Suppress("TooManyFunctions")
+
 package hwolf.validation.constraints
 
 import hwolf.validation.Constraint
@@ -6,60 +7,69 @@ import hwolf.validation.ConstraintBuilder
 
 // isEmpty
 
-/**
- * A constraint that validate if the value is empty
- */
+/** A constraint that validate if the value is empty. */
 object Empty : Constraint
 
+/** Validates if the [Iterable] property is empty. */
 @JvmName(name = "isEmptyIterable")
 fun <V> ConstraintBuilder<out Iterable<V>>.isEmpty() = validate(Empty) { it.count() == 0 }
 
+/** Validates if the [Collection] property is empty. */
 @JvmName(name = "isEmptyCollection")
 fun <V> ConstraintBuilder<out Collection<V>>.isEmpty() = validate(Empty) { it.isEmpty() }
 
+/** Validates if the [Array] property is empty. */
 @JvmName(name = "isEmptyArray")
 fun <V> ConstraintBuilder<Array<V>>.isEmpty() = validate(Empty) { it.isEmpty() }
 
+/** Validates if the [Map] property is empty. */
 @JvmName(name = "isEmptyMap")
 fun <K, V> ConstraintBuilder<out Map<K, V>>.isEmpty() = validate(Empty) { it.isEmpty() }
 
 
 // isNotEmpty
 
-/**
- * A constraint that validate if the value is not empty.
- */
+/** A constraint that validate if the value is not empty. */
 object NotEmpty : Constraint
 
+/** Validates if the [Iterable] property is not empty. */
 @JvmName(name = "isNotEmptyIterable")
 fun <V> ConstraintBuilder<out Iterable<V>>.isNotEmpty() = validate(NotEmpty) { it.count() > 0 }
 
+/** Validates if the [Collection] property is not empty. */
 @JvmName(name = "isNotEmptyCollection")
 fun <V> ConstraintBuilder<out Collection<V>>.isNotEmpty() = validate(NotEmpty) { it.isNotEmpty() }
 
+/** Validates if the [Array] property is not empty. */
 @JvmName(name = "isNotEmptyArray")
 fun <V> ConstraintBuilder<Array<V>>.isNotEmpty() = validate(NotEmpty) { it.isNotEmpty() }
 
+/** Validates if the [Map] property is not empty. */
 @JvmName(name = "isNotEmptyMap")
 fun <K, V> ConstraintBuilder<out Map<K, V>>.isNotEmpty() = validate(NotEmpty) { it.isNotEmpty() }
 
 
 // hasSize
 
+/** a constraint that validate if the size of value is within the limits (min and max). */
 data class Size(val min: Int, val max: Int) : Constraint
 
+/**  Validates if the [Iterable] property size is within the limits (min and max). */
 @JvmName(name = "hasSizeIterable")
 fun <V> ConstraintBuilder<out Iterable<V>>.hasSize(min: Int, max: Int) =
     validate(Size(min, max)) { it.count() in min..max }
 
+/**  Validates if the [Collection] property size is within the limits (min and max). */
 @JvmName(name = "hasSizeCollection")
 fun <V> ConstraintBuilder<out Collection<V>>.hasSize(min: Int, max: Int) =
     validate(Size(min, max)) { it.size in min..max }
 
+/**  Validates if the [Array] property size is within the limits (min and max). */
 @JvmName(name = "hasSizeArray")
 fun <V> ConstraintBuilder<Array<V>>.hasSize(min: Int, max: Int) =
     validate(Size(min, max)) { it.size in min..max }
 
+/**  Validates if the [Map] property size is within the limits (min and max). */
 @JvmName(name = "hasSizeMap")
 fun <K, V> ConstraintBuilder<out Map<K, V>>.hasSize(min: Int, max: Int) =
     validate(Size(min, max)) { it.size in min..max }
