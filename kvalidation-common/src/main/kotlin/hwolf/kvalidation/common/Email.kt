158511d34a58f07fde4c7a3ae3e04ff4e5567129
hwolf <heiko>
2021-12-15 21:32:45 +0100

    Add API documentation

--- kvalidation-common/src/main/kotlin/hwolf/kvalidation/common/Email.kt
+++ kvalidation-common/src/main/kotlin/hwolf/kvalidation/common/Email.kt
@@ -4,6 +4,7 @@ import hwolf.kvalidation.Constraint
 import hwolf.kvalidation.ConstraintBuilder
 import org.apache.commons.validator.routines.EmailValidator
 
+/** A constraint that validate if the value is an email. */
 data class Email(
     val options: Collection<Options>
 ) : Constraint {
@@ -14,10 +15,13 @@ data class Email(
     }
 }
 
+/** Validates if the property value is an email. */
 fun ConstraintBuilder<String>.isEmail() = isEmail(emptySet())
 
+/** Validates if the property value is an email. */
 fun ConstraintBuilder<String>.isEmail(vararg options: Email.Options) = isEmail(options.toSet())
 
+/** Validates if the property value is an email. */
 fun ConstraintBuilder<String>.isEmail(options: Collection<Email.Options>) = validate(Email(options)) {
     EmailValidator.getInstance(Email.Options.AllowLocal in options, false).isValid(it)
 }
--- kvalidation-common/src/main/kotlin/hwolf/kvalidation/common/Url.kt
+++ kvalidation-common/src/main/kotlin/hwolf/kvalidation/common/Url.kt
@@ -4,8 +4,10 @@ import hwolf.kvalidation.Constraint
 import hwolf.kvalidation.ConstraintBuilder
 import org.apache.commons.validator.routines.UrlValidator
 
+/** A constraint that validate if the value is a URL. */
 object Url : Constraint
 
+/** Validates if the property value is a URL. */
 fun ConstraintBuilder<String>.isUrl() = validate(Url) {
     UrlValidator.getInstance().isValid(it)
 }
