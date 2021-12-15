be6d5d22bf5881fbfb23bd0d60382ee0d18202f1
hwolf <heiko>
2021-12-15 21:10:11 +0100

    Releases only from main branch

--- kvalidation-common/src/main/kotlin/hwolf/kvalidation/common/Email.kt
+++ kvalidation-common/src/main/kotlin/hwolf/kvalidation/common/Email.kt
@@ -4,8 +4,19 @@ import hwolf.kvalidation.Constraint
 import hwolf.kvalidation.ConstraintBuilder
 import org.apache.commons.validator.routines.EmailValidator
 
-object Email : Constraint
+data class Email(
+    val options: Collection<Options>
+) : Constraint {
+    enum class Options {
+        /** Should local addresses be considered valid ? */
+        AllowLocal
+    }
+}
 
-fun ConstraintBuilder<String>.isEmail() = validate(Email) {
-    EmailValidator.getInstance().isValid(it)
+fun ConstraintBuilder<String>.isEmail() = isEmail(emptySet())
+
+fun ConstraintBuilder<String>.isEmail(vararg options: Email.Options) = isEmail(options.toSet())
+
+fun ConstraintBuilder<String>.isEmail(options: Collection<Email.Options>) = validate(Email(options)) {
+    EmailValidator.getInstance(Email.Options.AllowLocal in options, false).isValid(it)
 }
---
+++ kvalidation-common/src/test/kotlin/hwolf/kvalidation/common/EmailTests.kt
@@ -0,0 +1,64 @@
+@file:Suppress("unused")
+
+package hwolf.kvalidation.common
+
+import dev.minutest.given
+import dev.minutest.rootContext
+import dev.minutest.test
+import hwolf.kvalidation.ConstraintViolation
+import hwolf.kvalidation.Validator
+import hwolf.kvalidation.validate
+import hwolf.kvalidation.validator
+import org.junit.platform.commons.annotation.Testable
+import strikt.api.expectThat
+
+private data class EmailBean(val email: String)
+
+private const val NORMAL_MAIL = "max.mustermann@muster.de"
+private const val LOCAL_MAIL = "max.mustermann@localhost"
+
+@Testable
+fun isEmail() = rootContext<Validator<EmailBean>> {
+    given {
+        validator { EmailBean::email { isEmail() } }
+    }
+    forEach(NORMAL_MAIL) { mail ->
+        test("$mail is valid") { validator ->
+            val actual = validator.validate(EmailBean(mail))
+            expectThat(actual).isValid()
+        }
+    }
+    forEach(LOCAL_MAIL, "max.mustermann@", "muster.xy") { mail ->
+        test("$mail is invalid") { validator ->
+            val actual = validator.validate(EmailBean(mail))
+            expectThat(actual).hasViolations(ConstraintViolation(
+                propertyName = "email",
+                propertyType = "String",
+                propertyValue = mail,
+                constraint = Email(emptySet())))
+        }
+    }
+}
+
+@Testable
+fun isLocalEmail() = rootContext<Validator<EmailBean>> {
+    given {
+        validator { EmailBean::email { isEmail(Email.Options.AllowLocal) } }
+    }
+    forEach(NORMAL_MAIL, LOCAL_MAIL) { mail ->
+        test("$mail is valid") { validator ->
+            val actual = validator.validate(EmailBean(mail))
+            expectThat(actual).isValid()
+        }
+    }
+    forEach("max.mustermann@", "muster.xy") { mail ->
+        test("$mail is invalid") { validator ->
+            val actual = validator.validate(EmailBean(mail))
+            expectThat(actual).hasViolations(ConstraintViolation(
+                propertyName = "email",
+                propertyType = "String",
+                propertyValue = mail,
+                constraint = Email(setOf(Email.Options.AllowLocal))))
+        }
+    }
+}
