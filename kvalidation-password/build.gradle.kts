bcff50bba3474a219d8621c7786aeed38b4f102d
hwolf <heiko>
2021-12-18 17:37:45 +0100

    Revert

--- kvalidation-password/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-apply(plugin = "java-library")
-apply(plugin = "org.jetbrains.dokka")
-
-dependencies {
-    implementation(project(":kvalidation-core"))
-
-    implementation("org.passay:passay:1.6.1")
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,5 @@ rootProject.name = "validation"
 
 include("kvalidation-core")
 include("kvalidation-common")
-include("kvalidation-password")
 include("kvalidation-bom")
 include("kvalidation-samples")
