e89d60744095ce96d808f6319508ebb663548efd
hwolf <heiko>
2022-01-27 22:55:23 +0100

    Migrate tests to kotest

--- kvalidation-core/src/test/kotlin/hwolf/kvalidation/ConstraintTests.kt
+++ kvalidation-core/src/test/kotlin/hwolf/kvalidation/ConstraintTests.kt
@@ -1,18 +1,12 @@
 package hwolf.kvalidation
 
-import dev.minutest.given
-import dev.minutest.rootContext
-import dev.minutest.test
-import org.junit.platform.commons.annotation.Testable
+import io.kotest.core.spec.style.FunSpec
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
-@Testable
-fun testsConstraint() = rootContext<TestConstraint> {
-    given {
-        TestConstraint(prop1 = "Value 1", prop2 = null)
-    }
+class ConstraintTests : FunSpec({
     test("Message key is hwolf.validation.TestConstraint") {
-        expectThat(this.messageKey).isEqualTo("hwolf.kvalidation.TestConstraint")
+        val constraint = TestConstraint(prop1 = "Value 1", prop2 = null)
+        expectThat(constraint.messageKey).isEqualTo("hwolf.kvalidation.TestConstraint")
     }
-}
+})
--- kvalidation-core/src/test/kotlin/hwolf/kvalidation/MessageInterpolatorTest.kt
+++ kvalidation-core/src/test/kotlin/hwolf/kvalidation/MessageInterpolatorTest.kt
@@ -1,52 +1,41 @@
 package hwolf.kvalidation
 
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments.of
-import org.junit.jupiter.params.provider.MethodSource
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@Suppress("MemberVisibilityCanBePrivate")
-class MessageInterpolatorTest {
+class MessageInterpolatorTest : FunSpec({
 
-    @ParameterizedTest
-    @MethodSource("messageSources")
-    fun `Order of codes for simple property`(messageSource: MessageSource, expected: String) {
-
-        // Given
-        val sut = MessageInterpolator(
-            messageSource = messageSource,
-            messageFormatter = { template, _, _ -> template },
-            messageCodeResolver = { listOf("a.b.c", "a.b", "a") })
-
-        // When
-        val actual = sut.interpolate(constraintViolation(), Locale.GERMAN)
-
-        // Then
-        expectThat(actual).isEqualTo(expected)
-    }
-
-    private fun constraintViolation() = ConstraintViolation(
+    fun constraintViolation() = ConstraintViolation(
         propertyPath = listOf(PropertyName("test")),
         propertyType = PropertyType("String"),
         propertyValue = "xxx",
         constraint = Equal(1234))
 
-    fun messageSources() = listOf(
-        of(messageSourceMap(
-            "a" to "Level 3",
-            "a.b" to "Level 2",
-            "a.b.c" to "Level 1"),
-            "Level 1"),
-        of(messageSourceMap(
-            "a" to "Level 3",
-            "a.b" to "Level 2"),
-            "Level 2"),
-        of(messageSourceMap(
-            "a" to "Level 3"),
-            "Level 3"),
-        of(messageSourceMap(),
-            "hwolf.kvalidation.Equal"))
-}
+    context("Order of codes for simple property") {
+        withData(
+            Pair(messageSourceMap(
+                "a" to "Level 3",
+                "a.b" to "Level 2",
+                "a.b.c" to "Level 1"),
+                "Level 1"),
+            Pair(messageSourceMap(
+                "a" to "Level 3",
+                "a.b" to "Level 2"),
+                "Level 2"),
+            Pair(messageSourceMap(
+                "a" to "Level 3"),
+                "Level 3"),
+            Pair(messageSourceMap(),
+                "hwolf.kvalidation.Equal")
+        ) { (messageSource, expected) ->
+            val sut = MessageInterpolator(
+                messageSource = messageSource,
+                messageFormatter = { template, _, _ -> template },
+                messageCodeResolver = { listOf("a.b.c", "a.b", "a") })
+            val actual = sut.interpolate(constraintViolation(), Locale.GERMAN)
+            expectThat(actual).isEqualTo(expected)
+        }
+    }
+})
