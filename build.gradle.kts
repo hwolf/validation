b89615c31996c1659e445dcac7979a6c513408ae
hwolf <hwolf@users.noreply.github.com>
2023-07-29 11:26:17 +0200

    Task/migrate build to kts (#129)
    * Enable publishing to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
+    id("org.jetbrains.dokka")
     id("org.sonarqube") version "4.3.0.3225"
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0-rc-1"
+}
+
+repositories {
+    mavenCentral()
 }
 
 sonarqube {
@@ -10,5 +16,14 @@ sonarqube {
         property("sonar.coverage.jacoco.xmlReportPaths", "**/build/kover/result.xml")
         property("sonar.sources", "src/main")
         property("sonar.tests", "src/test")
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
     }
 }
