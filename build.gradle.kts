79d1335f47713bdadf90759fe4de720b719f2803
hwolf <heiko>
2023-10-10 14:55:07 +0200

    Try to fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ sonarqube {
         property("sonar.projectKey", "hwolf_kvalidation")
         property("sonar.organization", "hwolf")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.jacoco.xmlReportPaths", "**/build/kover/result.xml")
+        property("sonar.coverage.jacoco.xmlReportPaths", listOf("**/build/kover/result.xml"))
         property("sonar.sources", "src/main")
         property("sonar.tests", "src/test")
     }
